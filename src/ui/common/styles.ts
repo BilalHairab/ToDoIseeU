9ca87e43d87a15573551671874ae42d85675e855
Dania Sankar <bilal.hairab@gmail.com>
2025-07-04 18:59:27 +0400

    Fix background color clip

--- src/ui/common/styles.ts
+++ src/ui/common/styles.ts
@@ -3,6 +3,7 @@ import { StyleSheet } from 'react-native';
 const commonStyles = StyleSheet.create({
   mainScreenContainer: {
     flex: 1,
+    backgroundColor: '#fff',
   },
   centerElement: { flex: 1, justifyContent: 'center', alignItems: 'center' },
   errorElementTextStyle: { color: 'red' },
